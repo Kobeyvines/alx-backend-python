d6ea28dc502973016d09a3466e2093bc123b83ef
Brian Kuria <12995.2020@students.ku.ac.ke>
2025-07-11 15:39:15 +0300

    task 0: the return of task 0

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -3,6 +3,9 @@
 import sqlite3
 
 class DatabaseConnection:
+    def __init__(self):
+        self.conn = None
+
     def __enter__(self):
         self.conn = sqlite3.connect('users.db')
         return self.conn
