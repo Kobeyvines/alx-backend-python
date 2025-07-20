33af31baa06f02b81cf4916053d07d83ef353356
Brian Kuria <12995.2020@students.ku.ac.ke>
2025-07-20 16:17:16 +0300

    task 3: 001

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+# messaging_app/chats/views.py
 
-# Create your views here.
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+
+    def create(self, request, *args, **kwargs):
+        """
+        Create a new conversation with participants.
+        Expecting 'participants_id' in request.data
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+
+    def create(self, request, *args, **kwargs):
+        """
+        Send a message to an existing conversation.
+        Expecting 'sender_id', 'conversation_id', and 'message_body' in request.data
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
