1a946d5cf89a651e3701ae2a44f069ae21727391
Brian Kuria <12995.2020@students.ku.ac.ke>
2025-07-20 12:02:17 +0300

    task 3: 002

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,29 +1,10 @@
 #!/usr/bin/env python3
-"""Unit tests for utils.get_json
+"""Unit tests for utils.memoize
 """
 import unittest
-from unittest.mock import patch, Mock
-from parameterized import parameterized
-from utils import get_json
+from unittest.mock import patch
 from utils import memoize
 
-class TestGetJson(unittest.TestCase):
-    """Test cases for get_json."""
-
-    @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False})
-    ])
-    @patch('utils.requests.get')
-    def test_get_json(self, test_url, test_payload, mock_get):
-        """Test get_json returns expected payload with mocked requests.get."""
-        mock_response = Mock()
-        mock_response.json.return_value = test_payload
-        mock_get.return_value = mock_response
-
-        result = get_json(test_url)
-        mock_get.assert_called_once_with(test_url)
-        self.assertEqual(result, test_payload)
 class TestMemoize(unittest.TestCase):
     """Test cases for memoize decorator."""
 
