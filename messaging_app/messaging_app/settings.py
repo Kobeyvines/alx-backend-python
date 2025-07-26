89bbf9a2c7737ddf917e594389393452f9e87e94
Brian Kuria <12995.2020@students.ku.ac.ke>
2025-07-26 19:34:27 +0300

    Task 0: 002

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -131,12 +131,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication', 
     ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
     ],
 }
 
