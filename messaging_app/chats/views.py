9c01b029965de6de011d9307145b8d68401cc29a
Brian Kuria <12995.2020@students.ku.ac.ke>
2025-07-26 20:43:04 +0300

    task 1: 004

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -7,6 +7,13 @@ from .serializers import ConversationSerializer, MessageSerializer
 from .permissions import IsAuthenticatedAndParticipant
 from .filters import MessageFilter
 from django.shortcuts import get_object_or_404
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST,
+    HTTP_403_FORBIDDEN,
+    HTTP_404_NOT_FOUND
+)
 
 class ConversationViewSet(viewsets.ModelViewSet):
     """
@@ -19,6 +26,17 @@ class ConversationViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         conversation = serializer.save()
         conversation.participants.add(self.request.user)
+        return Response(serializer.data, status=HTTP_201_CREATED)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user not in instance.participants.all():
+            raise PermissionDenied("You are not a participant of this conversation")
 
 class MessageViewSet(viewsets.ModelViewSet):
     """
@@ -32,7 +50,14 @@ class MessageViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         conversation_id = self.kwargs.get('conversation_pk')
         if not conversation_id:
-            return Message.objects.none()  # Return empty queryset instead of Response
+            return Message.objects.none()
+        
+        conversation = get_object_or_404(Conversation, id=conversation_id)
+        if self.request.user not in conversation.participants.all():
+            raise PermissionDenied(
+                detail="You are not a participant of this conversation",
+                code=HTTP_403_FORBIDDEN
+            )
         return Message.objects.filter(conversation_id=conversation_id)
 
     def perform_create(self, serializer):
@@ -41,8 +66,17 @@ class MessageViewSet(viewsets.ModelViewSet):
             id=self.kwargs.get('conversation_pk')
         )
         if self.request.user not in conversation.participants.all():
-            raise PermissionDenied("You are not a participant of this conversation")
+            raise PermissionDenied(
+                detail="You are not a participant of this conversation",
+                code=HTTP_403_FORBIDDEN
+            )
         serializer.save(
             sender=self.request.user,
             conversation=conversation
         )
+        return Response(serializer.data, status=HTTP_201_CREATED)
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
