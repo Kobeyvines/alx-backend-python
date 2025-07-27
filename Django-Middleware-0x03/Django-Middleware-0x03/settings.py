9f7cb7196b0c9ec6879a4fb8745a16a192c3f070
Brian Kuria <12995.2020@students.ku.ac.ke>
2025-07-27 18:32:52 +0300

    task 4:002:Enforce chat user Role Permissions

--- Django-Middleware-0x03/Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/Django-Middleware-0x03/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",  # This must come before our middleware
-    "chats.middleware.RolePermissionMiddleware",  # Role-based access control
+    "chats.middleware.RolepermissionMiddleware",  # Role-based access control
     "chats.middleware.RestrictAccessByTimeMiddleware",  # Time restriction middleware
     "chats.middleware.OffensiveLanguageMiddleware",  # Rate limiting middleware
     "chats.middleware.RequestLoggingMiddleware",  # Our custom logging middleware
